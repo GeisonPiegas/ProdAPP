d328454a833730abc102ed9a90abecd2bb744e88
Geison Goulart Piegas <geisonpiegas2018@gmail.com>
2017-05-25 15:55:41 -0300

    Toma então

---
+++ app/src/main/java/Putao/TripliceAndroiod.java
@@ -0,0 +1,14 @@
+package Putao;
+
+import android.view.View;
+
+/**
+ * Created by Alunos on 25/05/2017.
+ */
+
+public class TripliceAndroiod implements View.OnClickListener {
+    @Override
+    public void onClick(View v) {
+
+    }
+}
