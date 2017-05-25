ca274a35f0e82e5495702162421c6ddbcd060874
Skema <skma1114@hotmail.com>
2017-05-25 17:09:56 -0300

    Criação da Tela de Login

---
+++ app/src/main/java/com/example/alunos/prodapp/TelaDeLogin.java
@@ -0,0 +1,13 @@
+package com.example.alunos.prodapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TelaDeLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_de_login);
+    }
+}
