606cfc60bc72e190fc255248da0f70f6d21a174f
Geison Goulart Piegas <geisonpiegas2018@gmail.com>
2017-05-25 18:29:23 -0300

    Tela para escolha do SILO

---
+++ app/src/main/java/com/example/alunos/prodapp/TelaEscolhaSilo.java
@@ -0,0 +1,13 @@
+package com.example.alunos.prodapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TelaEscolhaSilo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_escolha_silo);
+    }
+}
