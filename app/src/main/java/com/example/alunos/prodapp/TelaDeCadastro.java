39f15059c396a23a0173e5ef344658584ce700a7
Rafael <skema1114@hotmail.com>
2017-05-26 18:46:22 -0300

    Variáveis da Tela de Cadastro do Usuario

--- app/src/main/java/com/example/alunos/prodapp/TelaDeCadastro.java
+++ app/src/main/java/com/example/alunos/prodapp/TelaDeCadastro.java
@@ -3,8 +3,30 @@ package com.example.alunos.prodapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 public class TelaDeCadastro extends AppCompatActivity implements View.OnClickListener{
+    private RelativeLayout RelativeLayoutTelaDeCadastro;
+    private ScrollView ScrollViewTelaDeCadastro;
+    private LinearLayout LinearLayoutVerticalTelaDeCadastro;
+    private ImageView IconeImageViewTelaDeCadastro;
+    private TextView NomeTextViewTelaDeCadastro;
+    private EditText NomeEditTextTelaDeCadastro;
+    private TextView EmailTextViewTelaDeCadastro;
+    private EditText EmailEditTextTelaDeCadastro;
+    private TextView ConfirmarEmailTextViewTelaDeCadastro;
+    private EditText ConfirmarEmailEditTextTelaDeCadastro;
+    private TextView SenhaTextViewTelaDeCadastro;
+    private EditText SenhaEditTextTelaDeCadastro;
+    private TextView ConfirmarSenhaTextViewTelaDeCadastro;
+    private EditText ConfirmarSenhaEditTextTelaDeCadastro;
+    private Button CadastrarButtonTelaDeCadastro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
