c4781a99ffa6113283ad48f5af76dbdd5e6a5b3b
Geison Goulart Piegas <geisonpiegas2018@gmail.com>
2017-06-11 20:54:27 -0300

    Pequenas mudanças

--- app/src/main/java/com/example/alunos/prodapp/TelaEpocasDePlantacao.java
+++ app/src/main/java/com/example/alunos/prodapp/TelaEpocasDePlantacao.java
@@ -1,9 +1,13 @@
 package com.example.alunos.prodapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -20,6 +24,40 @@ public class TelaEpocasDePlantacao extends AppCompatActivity {
         GridView lista = (GridView) findViewById(R.id.GridViewTelaListaProdutos);
         ArrayAdapter adapter = new AdapterProdutos(this, adicionarProdutos());
         lista.setAdapter(adapter);
+
+        lista.setOnItemClickListener(new GridView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView parent, View view, int position, long id) {
+                // Toast: Mostra uma menssagem na tela com a posição da imagem, so pra teste!
+                Toast.makeText(getBaseContext(), "Imagem" + position, Toast.LENGTH_SHORT).show();
+
+                //IF: Fazem o teste de qual posição o usuario clickou, e leva para a pagina indicada.
+                //Ele pega a POSIÇÂO do click e nao ID, e a posição comeca em "0"
+                if(position == 0){
+                    Intent intent = new Intent(TelaEpocasDePlantacao.this, ListaSilos.class) ;
+                    startActivity(intent);
+                    finish();
+                }else{
+                    if(position == 1){
+                        Intent intent = new Intent(TelaEpocasDePlantacao.this, ListaSilos.class) ;
+                        startActivity(intent);
+                        finish();
+                    }else{
+                        if(position == 2){
+                            Intent intent = new Intent(TelaEpocasDePlantacao.this, ListaSilos.class) ;
+                            startActivity(intent);
+                            finish();
+                        }else{
+                            if(position == 3){
+                                Intent intent = new Intent(TelaEpocasDePlantacao.this, ListaSilos.class) ;
+                                startActivity(intent);
+                                finish();
+                            }
+                        }
+                    }
+                }
+            }
+        });
 
     }
 
