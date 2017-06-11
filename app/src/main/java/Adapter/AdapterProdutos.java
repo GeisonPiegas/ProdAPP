74d7743329b00bc25e5e4782cfe907f0671dc354
Geison Goulart Piegas <geisonpiegas2018@gmail.com>
2017-06-11 20:45:42 -0300

    Parte de reconhecer a posição da imagem clickada

---
+++ app/src/main/java/Adapter/AdapterProdutos.java
@@ -0,0 +1,52 @@
+package Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.alunos.prodapp.ListaSilos;
+import com.example.alunos.prodapp.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import Interacao.Produtos;
+
+/**
+ * Created by Geison on 11/06/2017.
+ */
+
+public class AdapterProdutos extends ArrayAdapter<Produtos> {
+    private final Context context;
+    private final ArrayList<Produtos> elementos;
+
+
+    public AdapterProdutos(Context context, ArrayList<Produtos> elementos){
+        super(context, R.layout.item_produtos,(List<Produtos>) elementos);
+        this.context = context;
+        this.elementos = elementos;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent){
+        Produtos produtos = elementos.get(position);
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View rowView = inflater.inflate(R.layout.item_produtos, parent, false);
+
+        ImageView imagem = (ImageView) rowView.findViewById(R.id.ImagemViewTelaListaProdutos);
+        TextView produto = (TextView) rowView.findViewById(R.id.TextViewTelaListaProdutos);
+
+        imagem.setImageResource(elementos.get(position).getImagem());
+        produto.setText(elementos.get(position).getProduto());
+
+        return rowView;
+    }
+
+
+
+
+}
--- app/src/main/java/Adapter/AdapterUsuarios.java
+++ app/src/main/java/Adapter/AdapterUsuarios.java
@@ -12,6 +12,7 @@ import com.example.alunos.prodapp.R;
 
 import java.util.List;
 
+import Interacao.Silos;
 import Interacao.Usuarios;
 
 /**
@@ -21,11 +22,11 @@ public class AdapterUsuarios extends BaseAdapter {
 
     //Criando novos objetos
     private Context context;
-    private List<Usuarios> lista;
+    private List<Silos> lista;
 
 
     //Criando o metodo construtor
-    public AdapterUsuarios(Context ctx, List<Usuarios> usuarios) { // Recebendo por parametro e setando.
+    public AdapterUsuarios(Context ctx, List<Silos> usuarios) { // Recebendo por parametro e setando.
         this.context = ctx;
         this.lista = usuarios;
     }
@@ -50,7 +51,7 @@ public class AdapterUsuarios extends BaseAdapter {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
 
-        Usuarios usuario = lista.get(position);
+        Silos usuario = lista.get(position);
 
         if (convertView == null) { //testando um componente grafico que está sendo pego pelo "getView" acima
 
@@ -64,7 +65,7 @@ public class AdapterUsuarios extends BaseAdapter {
         /* AINDA NÃO É POSSIVEL CHAMAR INFORMAÇÕES DE OUTRAS TABELAS E ENVIAR PARA O TEXTVIEW E LISTVIEW  */
 
         TextView textView = (TextView) convertView.findViewById(R.id.textViewnomeSilo);
-        textView.setText("ID : "+ usuario.getId_usuario()+"\n\nNOME: " + usuario.getNome_usuario() + "\n\nE-MAIL: " + usuario.getEmail_usuario()+"  \n\n");
+        textView.setText("ID : "+ usuario.getId_silo()+"\n\nNOME: " + usuario.getNome_silo() + "\n\nProduto: " + usuario.getProduto_silo()+"  \n\nTamanho:" + usuario.getTamanho_silo() +"  \n\n");
 
         return convertView ;
     }
