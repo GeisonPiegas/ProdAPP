db8365be37b23e1bbe59e5fc8c87c3492899cb23
Geison Goulart Piegas <geisonpiegas2018@gmail.com>
2017-08-10 22:57:54 -0300

    Um pouco mais da parte de descarte de agrotoxicos

--- app/src/main/java/com/example/alunos/prodapp/TelaComoDescartar.java
+++ app/src/main/java/com/example/alunos/prodapp/TelaComoDescartar.java
@@ -2,12 +2,19 @@ package com.example.alunos.prodapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class TelaComoDescartar extends AppCompatActivity {
+public class TelaComoDescartar extends AppCompatActivity implements View.OnClickListener
+{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_como_descartar);
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
