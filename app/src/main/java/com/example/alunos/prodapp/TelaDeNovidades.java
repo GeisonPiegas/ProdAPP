9fc3c184ca7f0e3f43d302c869b0048988d78af1
Rafael <skema1114@hotmail.com>
2017-08-17 10:58:04 -0300

    Criação de menu de contatos no feed de Novidades e adição de um menu superior na mesma activity.

--- app/src/main/java/com/example/alunos/prodapp/TelaDeNovidades.java
+++ app/src/main/java/com/example/alunos/prodapp/TelaDeNovidades.java
@@ -1,16 +1,24 @@
 package com.example.alunos.prodapp;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ScrollView;
 
 public class TelaDeNovidades extends AppCompatActivity implements View.OnClickListener{
     private ImageButton imageButtonAgroeste;
     private ImageButton imageButtonDeklab;
     private ImageButton imageButtonDimicron;
     private ImageButton imageButtonSementesEstrela;
+
+    private Button buttonNovidades;
+    private Button buttonForcedores;
+    private ScrollView ScrollViewNovidades;
+    private ScrollView ScrollViewFornecedores;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +29,13 @@ public class TelaDeNovidades extends AppCompatActivity implements View.OnClickLi
         imageButtonDeklab = (ImageButton) findViewById(R.id.imageButtonDeklab);
         imageButtonDimicron = (ImageButton) findViewById(R.id.imageButtonDimicron);
         imageButtonSementesEstrela = (ImageButton) findViewById(R.id.imageButtonSementesEstrela);
+        buttonNovidades = (Button) findViewById(R.id.buttonNovidades);
+        buttonForcedores = (Button) findViewById(R.id.buttonForcedores);
+        ScrollViewNovidades = (ScrollView) findViewById(R.id.ScrollViewNovidades);
+        ScrollViewFornecedores = (ScrollView) findViewById(R.id.ScrollViewFornecedores);
 
+        buttonForcedores.setOnClickListener(this);
+        buttonNovidades.setOnClickListener(this);
         imageButtonAgroeste.setOnClickListener(this);
         imageButtonDeklab.setOnClickListener(this);
         imageButtonDimicron.setOnClickListener(this);
@@ -45,6 +59,26 @@ public class TelaDeNovidades extends AppCompatActivity implements View.OnClickLi
         if (imageButtonSementesEstrela.isPressed()) {
             Intent intentSementesEstreka = new Intent(this, NoticiaSementesEstrela.class);
             startActivity(intentSementesEstreka);
+        }
+        if (buttonNovidades.isPressed()) {
+            ScrollViewNovidades.setVisibility(View.VISIBLE);
+            ScrollViewFornecedores.setVisibility(View.GONE);
+
+            buttonNovidades.setBackgroundColor(getResources().getColor(R.color.MenuNoticias));
+            buttonForcedores.setBackgroundColor(getResources().getColor(R.color.PadraoBotao));
+
+            buttonNovidades.setTypeface(null, Typeface.BOLD);
+            buttonForcedores.setTypeface(null, Typeface.NORMAL);
+        }
+        if (buttonForcedores.isPressed()) {
+            ScrollViewFornecedores.setVisibility(View.VISIBLE);
+            ScrollViewNovidades.setVisibility(View.GONE);
+
+            buttonForcedores.setBackgroundColor(getResources().getColor(R.color.MenuNoticias));
+            buttonNovidades.setBackgroundColor(getResources().getColor(R.color.PadraoBotao));
+
+            buttonForcedores.setTypeface(null, Typeface.BOLD);
+            buttonNovidades.setTypeface(null, Typeface.NORMAL);
         }
 
     }
