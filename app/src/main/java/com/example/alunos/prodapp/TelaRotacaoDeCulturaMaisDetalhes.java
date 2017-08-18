cfefd803f14ad47a78e57cde54c793d2cbdcfe74
Marcos <gronytzki@gmail.com>
2017-08-18 02:02:27 -0300

    Criando a tela com mais detalhes da Rotação de Silo

---
+++ app/src/main/java/com/example/alunos/prodapp/TelaRotacaoDeCulturaMaisDetalhes.java
@@ -0,0 +1,13 @@
+package com.example.alunos.prodapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TelaRotacaoDeCulturaMaisDetalhes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_rotacao_de_cultura_mais_detalhes);
+    }
+}
